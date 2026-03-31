02d4289cfc4100f80aaafdbe7ad08357150ee819
CheloStudio1 <estudiosamuelperalta@gmail.com>
2026-03-31 12:56:20 -0300

    chore: update Open Graph image alt text in layout metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
         url: "/og-image.png",
         width: 1200,
         height: 630,
-        alt: "CheloStudio Logo",
+        alt: "Chelo Std Logo",
       },
     ],
     locale: "es_ES",
